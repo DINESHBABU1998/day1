27dea9d1d8350394618da5e821c40b7797eafab1
DINESHBABU1998 <98023477+DINESHBABU1998@users.noreply.github.com>
2022-03-04 14:35:29 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,82 @@
+// var a -declaring the variable
+// var a = 12; - we are intializing the variable with value of 12
+//Decision making
+// if else(one condition)
+// let a = 10;
+// if(a == 10){
+//     console.log(success);
+// }
+// else{
+//     console.log(failure);
+// }
+// if else ladder(two condition)
+// let a = -10;
+// if (a<0){
+//     console.log("negative");
+// }
+// else if(a==0){
+//     console.log("zero")
+// }
+// else{
+//     console.log("positive")
+// }
+//switch case statement(three or more condition)
+// let gender = "m";
+// switch(gender){
+//     case "m":
+//     console.log("male");
+//     break;
+//     case "m":
+//     console.log("male");
+//     break;
+//     case "m":
+//     console.log("male");
+//     break;    
+// }
+//practice que 1
+// let age = 10;
+// switch(true){
+//     case (age<18):
+//         console.log("child");
+//         break;
+//     case (18<age<40):
+//         console.log("adult");
+//         break;
+//     default:
+//         console.log("old age");
+//         break;
+// }
+// practice que 2
+// let day="1";
+// switch(day){
+//     case "0":
+//     console.log("monday");
+//     break;
+//     case "1":
+//     console.log("tuesday");
+//     break;
+//     case "2":
+//     console.log("wednesday");
+//     break;
+//     case "3":
+//     console.log("thursday");
+//     break;
+//     case "4":
+//     console.log("friday");
+//     break;
+//     default:
+//     console.log("saturday");
+//     break;
+// }
+// Loops (Iteration statements)
+// for,while,do while
+
+//for - it has 3 values(initialization,condition,increment or decrement)
+
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
+for (let i=1 ; i<=10; i++){
+    console.log(19*i +"=" +19*i);
+}
+
